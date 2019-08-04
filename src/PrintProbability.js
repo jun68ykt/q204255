54c634802a4ff2256750fcef419ef6761a9bbec4
jun68ykt <jun68ykt@teratail>
2019-08-04 21:48:02 +0900

    エンプティステートを返す条件に合致したときに、すぐにreturnさせる

--- src/PrintProbability.js
+++ src/PrintProbability.js
@@ -5,10 +5,10 @@ class PrintProbability extends React.Component {
 
     const { probability, roletype } = this.props
 
-    return !isNaN(probability)
-             ? <div>{roletype}確率 :<br/> 1/{(probability).toFixed(1)}<hr/></div>
-             : <div>未入力です。</div>
+    if (isNaN(probability))
+      return  <div>未入力です。</div>
 
+    return  <div>{roletype}確率 :<br/> 1/{(probability).toFixed(1)}<hr/></div>
   }
 }
 
