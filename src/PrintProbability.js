12e276b6b15d5134149221a7bc5708eee3e4b905
jun68ykt <jun68ykt@teratail>
2019-08-04 20:41:30 +0900

    以下の2点をリファクタ
    ・isNaNのときにエンプティステートを返す処理に三項演算子を使用
    ・propsからの値取得に分割代入を使用

--- src/PrintProbability.js
+++ src/PrintProbability.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 class PrintProbability extends React.Component {
-  render(){
-    return(
-      (()=>{
-        if(!isNaN(this.props.probability)){
-          return <div>{this.props.roletype}確率 :<br/> 1/{(this.props.probability).toFixed(1)}<hr/></div>
-        }else{
-          return <div>未入力です。</div>
-        }
-      })()
-  )
+  render() {
+
+    const { probability, roletype } = this.props
+
+    return !isNaN(probability)
+             ? <div>{roletype}確率 :<br/> 1/{(probability).toFixed(1)}<hr/></div>
+             : <div>未入力です。</div>
+
   }
 }
 
