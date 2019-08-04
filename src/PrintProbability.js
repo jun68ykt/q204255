3058da861764001b9ba3aeebf1b697276e4bf9bf
jun68ykt <jun68ykt@teratail>
2019-08-04 20:29:02 +0900

    即時関数について以下2点を修正
    ・不要な { } を削除
    ・return を追加

--- src/PrintProbability.js
+++ src/PrintProbability.js
@@ -3,13 +3,13 @@ import React from 'react'
 class PrintProbability extends React.Component {
   render(){
     return(
-      {(()=>{
+      (()=>{
         if(!isNaN(this.props.probability)){
-          <div>{this.props.roletype}確率 :<br/> 1/{(this.props.probability).toFixed(1)}<hr/></div>
+          return <div>{this.props.roletype}確率 :<br/> 1/{(this.props.probability).toFixed(1)}<hr/></div>
         }else{
-          <div>未入力です。</div>
+          return <div>未入力です。</div>
         }
-      })()}
+      })()
   )
   }
 }
